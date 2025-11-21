e71c3c3669184e2e67e8bb627aff572783befc6c
Vatsal Mandloi <122007213+VatM@users.noreply.github.com>
2025-11-21 17:07:22 +0530

    MWF-2936 (MWF-2919) update library to support new architecture of react native (#2)
    - Updated native module class name to the supported convention to make it compatible with old and new react native architecture.

--- android/src/main/java/com/tectiv3/aes/Aes.java
+++ android/src/main/java/com/tectiv3/aes/Aes.java
@@ -38,20 +38,20 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class RCTAes extends ReactContextBaseJavaModule {
+public class Aes extends ReactContextBaseJavaModule {
 
     private static final String CIPHER_ALGORITHM = "AES/CBC/PKCS7Padding";
     public static final String HMAC_SHA_256 = "HmacSHA256";
     public static final String HMAC_SHA_512 = "HmacSHA512";
     private static final String KEY_ALGORITHM = "AES";
 
-    public RCTAes(ReactApplicationContext reactContext) {
+    public Aes(ReactApplicationContext reactContext) {
         super(reactContext);
     }
 
     @Override
     public String getName() {
-        return "RCTAes";
+        return "Aes";
     }
 
     @ReactMethod
